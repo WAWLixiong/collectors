c447d18112596ba747f8b16acece1a9d597af910
WAWLixiong <17601241441@163.com>
2019-07-18 23:45:36 +0800

    new:学习select多路复用编程 coroutine概念

---
+++ lean_python/lixiong/learn_coroutine/learn_concept.py
@@ -0,0 +1,20 @@
+# 什么是协程，即可以暂停的函数，可以往暂停的地方传入值
+
+
+def gen_fun():
+    #这行代码的作用
+    # 1 产出值，2 接收值(调用方传递进来的值)
+    html = yield 'http://www.baidu.com'
+    print(html)
+    yield 2
+    yield 3
+    return 4
+
+if __name__ == '__main__':
+    gen = gen_fun()
+    html = 'zzlion'
+    #send方法启动生成器，程序进程到下一个yield方法处
+    # 并传递值到赋值语句的右边
+    gen.send(None)
+    gen.send(html)
+
--- lean_python/lixiong/learn_io/learn_select_http.py
+++ lean_python/lixiong/learn_io/learn_select_http.py
@@ -1,3 +1,23 @@
+# ⣼⣯⠄⣸⣠⣶⣶⣦⣾⠄⡅⡅⠄⠄⠄⠄⡉⠹⠄⡅⠄⠄⠄
+# ⠿⠿⠶⠿⢿⣿⣿⣿⣿⣦⣤⣄⢀⡅⢠⣾⣛⡉⠄⠄⠄⠸⢀
+# ⣴⣶⣶⡀⠄⠄⠙⢿⣿⣿⣿⣿⣿⣴⣿⣿⣿⢃⣤⣄⣀⣥⣿
+# ⣿⣿⣿⣧⣀⢀⣠⡌⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠿⣿⣿
+# ⣤⣤⣤⣬⣙⣛⢿⣿⣿⣿⣿⣿⣿⡿⣿⣿⡍⠄⠄⢀⣤⣄⠉
+# ⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⢇⣿⣿⡷⠶⠶⢿⣿⣿⠇
+# ⣿⣿⣿⣿⣿⣿⣿⣿⣽⣿⣿⣿⡇⣿⣿⣿⣿⣿⣿⣷⣶⣥⣴
+# ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+# ⣻⣿⣿⣧⠙⠛⠛⡭⠅⠒⠦⠭⣭⡻⣿⣿⣿⣿⣿⣿⣿⣿⡿
+# ⣿⣿⣿⣿⡆⠄⠄⠄⠄⠄⠄⠄⠄⠹⠈⢋⣽⣿⣿⣿⣿⣵⣾
+# ⣿⣿⣿⣿⣿⠄⣴⣿⣶⣄⠄⣴⣶⠄⢀⣾⣿⣿⣿⣿⣿⣿⠃
+# ⠛⢿⣿⣿⣿⣦⠁⢿⣿⣿⡄⢿⣿⡇⣸⣿⣿⠿⠛⠁⠄⠄⠄
+# ⠄⠄⠉⠻⣿⣿⣿⣦⡙⠻⣷⣾⣿⠃⠿⠋⠁⠄⠄⠄⠄⠄⢀
+# ⣮⣥⠄⠄⠄⠛⢿⣿⣿⡆⣿⡿⠃⠄⠄⠄⠄⠄⠄⠄⣠⣴⣿
+
+#回调之痛
+#阅读困难
+#共享变量困难，需要作为实例变量
+#异常处理困难
+
 import socket
 from urllib.parse import urlparse
 # import select
@@ -10,6 +30,8 @@ from selectors import DefaultSelector, EVENT_READ, EVENT_WRITE
 
 
 selector = DefaultSelector()
+urls = ['http://www.baidu.com'] * 20
+stop = False
 
 # 使用select完成http请求
 class Fetcher:
@@ -18,26 +40,28 @@ class Fetcher:
         #回调函数中要取消注册的事件
         selector.unregister(key.fd)
         self.client.send("GET {} HTTP/1.1\r\nHost:{}\r\nConnection:close\r\n\r\n".format(self.path, self.host).encode('utf8'))
-        selector.register(self.client.fileno(), EVENT_READ, )
+        selector.register(self.client.fileno(), EVENT_READ, self.readable)
 
     def readable(self, key):
-        #回调函数中要取消注册的事件
-        selector.unregister(key.fd)
-        data = b""
-        while True:
-            try:
-                d = self.client.recv(1024)
-                if d:
-                    data += d
-                else:
-                    break
-            except BlockingIOError as e:
-                continue
-        html_data = data.decode('utf8').split('\r\n\r\n')[1]
-        print(html_data)
-        self.client.close()
+        # #回调函数中要取消注册的事件
+        # selector.unregister(key.fd)
+        d = self.client.recv(1024)
+        #编程readable之后，不代表可以一直从内核空间复制数据到用户空间
+        if d:
+            self.data += d
+        else:
+            selector.unregister(key.fd)
+            html_data = self.data.decode('utf8').split('\r\n\r\n')[1]
+            print(html_data)
+            self.client.close()
+            urls.remove(self.spider_url)
+            global stop
+            if not urls:
+                stop = True
 
     def get_url(self, url):
+        self.spider_url = url
+        self.data = b''
         url = urlparse(url)
         self.host = url.netloc
         self.path = url.path
@@ -64,6 +88,9 @@ class Fetcher:
 #回调需要自己调用
 #事件循环，虽然写了回调函数，但是不是在其可调用的时候，系统自动帮我们调用，而是需要我们手动控制回调
 def loop():
+    #事件循环，不停的请求socket的状态并调用对应的回调函数（twisted，tarndo，gevent，协程，asyncio）
+    #回调+事件循环+select(poll/epoll)
+
     # 1.
     # select 本身是不支持register模式的
     # selector 对 select封装后，支持register模式，
@@ -71,14 +98,20 @@ def loop():
     # 2.
     # socket状态变化以后的回调是由程序员完成的
     # key的数据类型，进入register方法查看源代码，SelectorKey = namedtuple('SelectorKey', ['fileobj', 'fd', 'events', 'data'])
-    while True:
-        ready = selector.select()
+    while not stop:
+        ready = selector.select() # 这个地方在windows下会报错(设置一个全局变量，请求的url结束以后，退出循环则不会报错)，
+        # linux下epoll不会报错
         for key, mask in ready:
             call_back = key.data
             call_back(key)
 
 if __name__ == '__main__':
-    feacher = Fetcher()
-    feacher.get_url('http://www.baidu.com')
+    import time
+    start = time.time()
+    for url in urls:
+        feacher = Fetcher()
+        feacher.get_url(url)
+    loop()
+    print(time.time() - start)
 
 
